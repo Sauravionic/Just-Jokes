cb3b6ee0ea6857171a3f828780e37907f1fdbfee
sauravionic <srivastava2000saurav@gmail.com>
2022-11-14 20:52:47 +0530

    Created Spring Project. Closes #1

---
+++ src/main/java/ionic/JustJokes/JustJokesApplication.java
@@ -0,0 +1,13 @@
+package ionic.JustJokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustJokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ionic/JustJokes/JustJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package ionic.JustJokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
